7d482674c322d47046a3aec07be1176500a095ce
togawamanabu <togawamanabu@gmail.com>
2017-08-02 12:28:53 +0900

    feat: replace static books to diyamic books

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import './App.css'
 class BooksApp extends React.Component {
   state = {
     books:[],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+      console.log(books)
+    })
   }
 
   render() {
@@ -21,9 +22,9 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route path="/search" component={SearchBook} />
 
-        <Route exact path="/" component={ListBooks} />
-
-
+        <Route exact path="/">
+          <ListBooks books={this.state.books} />
+        </Route>
       </div>
     )
   }
